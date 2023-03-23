ec7823e243d450dc3bf4e611d125858daab46d52
SeoMoonk <sunghoon1216@naver.com>
2023-03-23 13:08:33 +0900

    content_number_03_03

--- src/main/java/com/mysite/sbb/question/QuestionForm.java
+++ src/main/java/com/mysite/sbb/question/QuestionForm.java
@@ -8,8 +8,9 @@ import lombok.Setter;
 @Getter
 @Setter
 public class QuestionForm {
+
     @NotEmpty(message="제목은 필수 항목입니다.")
-    @Size(max=200)
+    @Size(max=200, message ="제목은 최대 200자 제한입니다.")
     private String subject;
 
     @NotEmpty(message="내용은 필수 항목입니다.")
